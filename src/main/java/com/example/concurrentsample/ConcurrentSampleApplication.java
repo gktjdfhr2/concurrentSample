e9ead84d72b1a821cf0eb02a801563b74816c262
하성록 <gktjdfhr2@naver.com>
2024-06-19 15:56:00 +0900

    🎉: "최초 커밋 "

---
+++ src/main/java/com/example/concurrentsample/ConcurrentSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrentsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentSampleApplication.class, args);
+    }
+
+}
