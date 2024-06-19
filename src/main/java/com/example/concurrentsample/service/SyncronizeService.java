4d5ade20446cfde3e197fb124a3abb5ac78c51cf
하성록 <gktjdfhr2@naver.com>
2024-06-19 16:10:40 +0900

    feat: ✨"자바 Syncronize 동시성 테스트"

---
+++ src/main/java/com/example/concurrentsample/service/SyncronizeService.java
@@ -0,0 +1,27 @@
+package com.example.concurrentsample.service;
+
+import com.example.concurrentsample.dto.MemberDto;
+import com.example.concurrentsample.entity.Member;
+import com.example.concurrentsample.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SyncronizeService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public synchronized void post(MemberDto memberDto) {
+        List<Member> all = memberRepository.findAll();
+
+        if(all.size() >= 30) {
+            return;
+        }
+        memberRepository.save(memberDto.toModel());
+    }
+}
